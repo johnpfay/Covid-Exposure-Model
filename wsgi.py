853148f1b6f79cee641e313c37ed477fdbc4c6c8
John Fay <john.fay@duke.edu>
2020-08-25 09:08:41 -0400

    moves text blocks to md files

--- wsgi.py
+++ wsgi.py
@@ -11,6 +11,10 @@ import plotly.express as px
 
 import pandas as pd
 import numpy as np
+#%% Read in web text objects
+HeaderText = open('HeaderText.md','r').read()
+CautionText = open('CautionText.md','r').read()
+CreditText = open('CreditText.md','r').read()
 
 #%% Functions
 def get_random(var,n=10000):
@@ -155,56 +159,9 @@ application = app.server
 #Construct the web site
 app.layout = html.Div([
     html.Div([
-    dcc.Markdown('''
-### Estimation of COVID-19 infection risk from airborne transmission during classroom teaching 
-
-This is a tool to help understand COVID-19 transmission risk to students and teachers/professors 
-due to [transmission by microscopic airborne particles 
-(i.e. aerosols)](https://science.sciencemag.org/content/368/6498/1422) in classroom settings.
-This is not an [infectious disease dynamics model](https://en.wikipedia.org/wiki/Compartmental_models_in_epidemiology), 
-but rather a model that predicts airborne 
-[virion](https://theconversation.com/what-is-a-virus-how-do-they-spread-how-do-they-make-us-sick-133437) 
-concentrations within a classroom, taking into account exhalation of 
-virion-containing aerosols by infected individuals and the loss of these particles due to various processes. 
-Probabilities of infection are calculated based on the
-virion dose inhaled (accounting for use of masks) by uninfected people in the classroom. 
-
-This probabilistic Monte Carlo framework was developed by 
-[Prasad Kasibhatla](https://nicholas.duke.edu/people/faculty/kasibhatla), 
-as an offshoot of the 
-[COVID-19 risk estimator](https://tinyurl.com/covid-estimator) developed by 
-[Jose Jimenez](https://www.colorado.edu/chemistry/jose-luis-jimenez).
-Please see the README and FAQ tabs on his [worksheet](https://tinyurl.com/covid-estimator) 
-for important information on assumptions, methodology, and inputs.
-The Monte Carlo capability implemented here allows for estimates of confidence 
-intervals for model predictions of infection probabilities.
-'''),
-dcc.Markdown('''
-_**Important**: The risk calculations here are only for disease transmission 
-by the airborne aerosol route, and do not account for transmission by droplets 
-or from contaminated surfaces. The implicit assumption is that appropriate 
-social distancing and hygiene protocols are strictly adhered to in the classroom. 
-To the extent that this is not true, the risk of infection will be higher than 
-predicted by these calculations. Users should also also bear in mind that the 
-absolute estimates of predicted risk from this model are quite uncertain because 
-of uncertainties in our knowledge of key parameters such as the exhalation rate 
-of virion-containing aerosols by infected individuals and the percentage of 
-infected individuals in the classroom. The model is nevertheless useful to 
-explore the relative effects of control measures (e.g. more ventilation, 
-fewer people, shorter duration, masks vs no masks) on COVID-19 transmission 
-by aerosols in classrooms._
-''',style={'color':'#cc0e0e'}),
-dcc.Markdown('''
----     
-Developed by [Prasad Kasibhatla](https://nicholas.duke.edu/people/faculty/kasibhatla), 
-[Jose Jimenez](https://www.colorado.edu/chemistry/jose-luis-jimenez), 
-[John Fay](https://nicholas.duke.edu/people/faculty/fay), 
-[Elizabeth Albright](https://nicholas.duke.edu/people/faculty/albright), and 
-[William Pan](https://nicholas.duke.edu/people/faculty/pan).  
-Please contact [Prasad Kasibhatla](mailto:psk9@duke.edu) with questions, comments, and suggestions.  
-Code availabile at: [https://github.com/johnpfay/Covid-Exposure-Model]
-
-'''),
+    dcc.Markdown(HeaderText),
+dcc.Markdown(CautionText,style={'color':'#cc0e0e'}),
+dcc.Markdown(CreditText),
 ],style={'border-style':'ridge',
            'border-radius': '5px',
            'padding':'0.5em',
